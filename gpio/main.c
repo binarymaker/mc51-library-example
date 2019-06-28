b67df28eab05398f86fee23f10417cc184140b38
Gokul Arunachalam <gok@lucastvs.co.in>
2019-06-28 14:06:14 +0530

    gpio example update

--- gpio/main.c
+++ gpio/main.c
@@ -2,8 +2,21 @@
 
 void main()
 {
+  
+  GPIO_PinMode(GPIO_PORT_1, GPIO_PIN_0 | GPIO_PIN_2, GPIO_MODE_OUTPUT);
+  GPIO_PinMode(GPIO_PORT_1, GPIO_PIN_1, GPIO_MODE_INPUT);
+  
   while(1)
   {
-    GPIO_WritePin(GPIO_PORT_1, GPIO_PIN_0, GPIO_PIN_RESET);
+    if (GPIO_ReadPin(GPIO_PORT_1, GPIO_PIN_1) == GPIO_PIN_RESET)
+    {
+      GPIO_WritePin(GPIO_PORT_1, GPIO_PIN_0, GPIO_PIN_RESET);
+    }
+    else
+    {
+      GPIO_WritePin(GPIO_PORT_1, GPIO_PIN_0, GPIO_PIN_SET);
+    }
+    
+    GPIO_TogglePin(GPIO_PORT_1, GPIO_PIN_2);
   }
 }
